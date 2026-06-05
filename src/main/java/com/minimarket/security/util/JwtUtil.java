22b274d2dee0e8f066d39347b7bfe330960c25dd
DaenNo1 <da.ericesn@duocuc.cl>
2026-06-05 12:15:56 -0400

    Update JwtUtil.java

--- src/main/java/com/minimarket/security/util/JwtUtil.java
+++ src/main/java/com/minimarket/security/util/JwtUtil.java
@@ -12,10 +12,11 @@ import io.jsonwebtoken.Claims;
 @Component
 public class JwtUtil {
 
+    //SECRET  es la contraseña con la que se firma el token, y EXPIRATION_MS es cuanto durará el token 
     private static final String SECRET = "minimarket-clave-secreta-super-segura-2024";
     private static final long EXPIRATION_MS =86400000;
 
-    //convierte el string secreto en un objeto secretKey
+    //convierte el string secreto en un objeto secretKey de esta manera la libreria JJWT lo puede entender 
     private SecretKey getSigninKey(){
         return Keys.hmacShaKeyFor(SECRET.getBytes());
     }
@@ -30,7 +31,7 @@ public class JwtUtil {
         return getClaims(token).getSubject();
     }
 
-    //intenta parsear el token, si la firma es invalida o esta expirado
+    //intenta parsear el token, si la firma es invalida o esta expirado, se lanza una excepcion y retorna un false, pero si todo esta bien, retorna true
     public boolean validateToken(String token){
         try{
             getClaims(token);
